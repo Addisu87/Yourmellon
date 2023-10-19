192c1c93cb4d44cbde7345ac63377328c22d619f
Addisu <addisuhaile87@gmail.com>
2023-10-19 11:21:45 +0300

    Update the Harshad method to identify the number type

---
+++ src/harshad.py
@@ -0,0 +1,17 @@
+def harshad(number):
+    sum_digits = sum(int(digit) for digit in str(number))
+    return number % sum_digits == 0
+
+
+def calculate():
+    try:
+        number = int(input("Enter an integer: "))
+        if harshad(number):
+            print(f"{number} is a Harshad number.")
+        else:
+            print(f"{number} is not a Harshad number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid input.")
+
+
+calculate()
