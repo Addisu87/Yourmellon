3d6c5c2b756b12c9e335069bc1fad06b82647e72
Addisu <addisuhaile87@gmail.com>
2023-10-19 16:22:54 +0300

    Add alternative name for harshad(niven) number

--- src/harshad.py
+++ src/harshad.py
@@ -7,9 +7,9 @@ def calculate():
     try:
         number = int(input("Enter an integer: "))
         if harshad(number):
-            print(f"{number} is a Harshad number.")
+            print(f"{number} is a Harshad(Niven) number.")
         else:
-            print(f"{number} is not a Harshad number.")
+            print(f"{number} is not a Harshad(Niven) number.")
     except ValueError:
         print("Invalid input. Please enter a valid input.")
 
